d01a50ba8ae25aadfa36ffd69e3d882dc9b99e6d
Le Thanh Long <longlt201203@gmail.com>
2025-09-16 11:44:58 +0700

    feat(auth): add validation decorators to PKCE token request DTO

--- src/modules/auth/dto/pkce-issue-token.request.ts
+++ src/modules/auth/dto/pkce-issue-token.request.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
 
 export class PkceIssueTokenRequest {
 	@ApiProperty()
+	@IsString()
 	codeVerifier: string;
 
 	@ApiProperty()
+	@IsString()
 	codeChallengeMethod: string;
 
 	@ApiProperty()
+	@IsString()
 	authCode: string;
 }
