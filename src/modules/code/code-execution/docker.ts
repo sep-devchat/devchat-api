7a2e57eab9e44005a1c369149bcae52cef04d7d0
Le Thanh Long <longlt201203@gmail.com>
2025-10-07 15:08:07 +0700

    refactor: streamline Docker class constructor and methods for improved readability

--- src/modules/code/code-execution/docker.ts
+++ src/modules/code/code-execution/docker.ts
@@ -1,6 +1,5 @@
 import { Env } from "@utils";
 import * as Dockerode from "dockerode";
-import * as fs from "fs";
 
 export class Docker {
 	private static instance: Docker;
@@ -16,10 +15,6 @@ export class Docker {
 				? {
 						host: Env.DOCKER_DIND_HOST,
 						port: Env.DOCKER_DIND_PORT,
-						protocol: "https",
-						ca: fs.readFileSync("/certs/ca.pem"),
-						cert: fs.readFileSync("/certs/cert.pem"),
-						key: fs.readFileSync("/certs/key.pem"),
 					}
 				: undefined,
 		);
