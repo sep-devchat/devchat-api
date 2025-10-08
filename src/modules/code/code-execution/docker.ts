aec6e08aedd870c79a1123c113796f8ec38182a4
Le Thanh Long <longlt201203@gmail.com>
2025-10-08 10:38:00 +0700

    feat: implement Java execution function with Docker support

--- src/modules/code/code-execution/docker.ts
+++ src/modules/code/code-execution/docker.ts
@@ -1,6 +1,7 @@
 import { Env } from "@utils";
 import * as Dockerode from "dockerode";
 import * as fs from "fs";
+import * as path from "path";
 
 export class Docker {
 	private static instance: Docker;
@@ -9,7 +10,9 @@ export class Docker {
 		return this.instance;
 	}
 
-	dockerode: Dockerode;
+	readonly dockerode: Dockerode;
+	readonly codeExecutionDir = path.join(process.cwd(), "code-execution-tmp");
+	readonly containerWorkingDir = "/app";
 	private constructor() {
 		this.dockerode = new Dockerode(
 			Env.USE_DOCKER_DIND
@@ -29,6 +32,14 @@ export class Docker {
 		return image.replace(/[:/]/g, "-");
 	}
 
+	getExecDir(runId: string) {
+		return `${this.codeExecutionDir}/${runId}`;
+	}
+
+	getContainerExecDir(runId: string) {
+		return `${this.containerWorkingDir}/${runId}`;
+	}
+
 	async pullImageIfNotExists(image: string) {
 		console.log("Checking for image:", image);
 		const images = await this.dockerode.listImages({
@@ -38,12 +49,12 @@ export class Docker {
 
 		if (images.length === 0) {
 			console.log("Pulling image:", image);
-			await this.dockerode.pull(image);
+			await this.dockerode.pull(`docker.io/${image}`);
 			console.log("Pulled image:", image);
 		}
 	}
 
-	async createExecContainer(image: string) {
+	async createExecContainer(image: string, runId?: string) {
 		await this.pullImageIfNotExists(image);
 
 		console.log("Creating container for image:", image);
@@ -52,9 +63,51 @@ export class Docker {
 			AttachStdout: true,
 			AttachStderr: true,
 			Tty: true,
+			WorkingDir: runId ? this.getContainerExecDir(runId) : undefined,
+			HostConfig: runId
+				? {
+						Binds: [
+							`${this.getExecDir(runId)}:${this.getContainerExecDir(runId)}`,
+						],
+					}
+				: undefined,
 		});
 		console.log("Created container for image:", image);
 
 		return container;
+	}
+
+	async cleanupContainer(container: Dockerode.Container, runId?: string) {
+		console.log("Stopping container...");
+		try {
+			await container.stop();
+		} catch (err) {
+			console.error("Error stopping container:", err);
+			console.log(err);
+			return;
+		}
+
+		console.log("Removing container...");
+		try {
+			await container.remove();
+			console.log("Removed container.");
+		} catch (err) {
+			console.error("Error removing container:", err);
+			console.log(err);
+			return;
+		}
+
+		if (runId) {
+			const execDir = this.getExecDir(runId);
+			console.log("Removing exec dir:", execDir);
+			try {
+				fs.rmSync(execDir, { recursive: true, force: true });
+				console.log("Removed exec dir.");
+			} catch (err) {
+				console.error("Error removing exec dir:", err);
+				console.log(err);
+				return;
+			}
+		}
 	}
 }
