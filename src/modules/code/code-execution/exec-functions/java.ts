a83bf11b68858b2e3a22e535e5357f7e0a0b6e7b
Le Thanh Long <longlt201203@gmail.com>
2025-12-06 15:42:50 +0700

    feat: update

--- src/modules/code/code-execution/exec-functions/java.ts
+++ src/modules/code/code-execution/exec-functions/java.ts
@@ -23,7 +23,7 @@ export const javaExecFunction: CodeExecutionFunction = async (code: string) => {
 	const compileResult = await docker.execCommand(
 		container,
 		runId,
-		["/bin/bash", "-c", "javac Main.java"],
+		["javac Main.java"],
 		20000,
 	);
 
@@ -33,8 +33,6 @@ export const javaExecFunction: CodeExecutionFunction = async (code: string) => {
 	if (!isTimeout) {
 		// Run
 		const execResult = await docker.execCommand(container, runId, [
-			"/bin/bash",
-			"-c",
 			"java Main",
 		]);
 		output += execResult.output;
